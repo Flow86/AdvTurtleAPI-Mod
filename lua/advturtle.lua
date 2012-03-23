0afe13a874e295766027dbdb9bd4f95b9b6089d1
Flow86 <sf-team@siedler25.org>
2012-03-23 11:59:54 +0100

    added more commands

--- lua/advturtle.lua
+++ lua/advturtle.lua
@@ -1,11 +1,22 @@
 
-if not advturtle then
-	error( "Cannot load advanced turtle API on computer" )
+if not advturtle or not turtle then
+	error( "Cannot load advanced turtle API on this computer" )
 end
 
 native = advturtle.native or advturtle
 
 local function wrap( _sCommand )
+	return function( ... )
+		local id = native[_sCommand]( ... )
+		local event, responseID, success
+		while event ~= "turtle_response" or responseID ~= id do
+			event, responseID, success = os.pullEvent( "turtle_response" )
+		end
+		return success
+	end
+end
+
+local function adv_wrap( _sCommand )
 	return function( ... )
 		local id = native[_sCommand]( ... )
 		local event, responseID, success
@@ -18,9 +29,50 @@ end
 
 local advturtle = {}
 
+-- original turtle api
+advturtle["forward"] = wrap( turtle.forward )
+advturtle["back"] = wrap( turtle.back )
+advturtle["up"] = wrap( turtle.up )
+advturtle["down"] = wrap( turtle.down )
+advturtle["turnLeft"] = wrap( turtle.turnLeft )
+advturtle["turnRight"] = wrap( turtle.turnRight )
+advturtle["select"] = wrap( turtle.select )
+advturtle["getItemCount"] = turtle.getItemCount
+advturtle["getItemSpace"] = turtle.getItemSpace
+advturtle["dig"] = wrap( turtle.dig )
+advturtle["digUp"] = wrap( turtle.digUp )
+advturtle["digDown"] = wrap( turtle.digDown )
+advturtle["place"] = wrap( turtle.place )
+advturtle["placeUp"] = wrap( turtle.placeUp )
+advturtle["placeDown"] = wrap( turtle.placeDown )
+advturtle["detect"] = wrap( turtle.detect )
+advturtle["detectUp"] = wrap( turtle.detectUp )
+advturtle["detectDown"] = wrap( turtle.detectDown )
+advturtle["compare"] = wrap( turtle.compare )
+advturtle["compareUp"] = wrap( turtle.compareUp )
+advturtle["compareDown"] = wrap( turtle.compareDown )
+advturtle["drop"] = wrap( turtle.drop )
+
+-- different spellings
+advturtle["turnleft"] = advturtle["turnLeft"]
+advturtle["turnright"] = advturtle["turnRight"]
+advturtle["getitemcount"] = advturtle["getItemCount"]
+advturtle["getitemspace"] = advturtle["getItemSpace"]
+advturtle["digup"] = advturtle["digUp"]
+advturtle["digdown"] = advturtle["digDown"]
+advturtle["placeup"] = advturtle["placeUp"]
+advturtle["placedown"] = advturtle["placeDown"]
+advturtle["detectup"] = advturtle["detectUp"]
+advturtle["detectdown"] = advturtle["detectDown"]
+advturtle["compareup"] = advturtle["compareUp"]
+advturtle["comparedown"] = advturtle["compareDown"]
+
+-- additional spellings and additional commands
 advturtle["findid"] = native["findBlockID"]
 advturtle["findblockid"] = native["findBlockID"]
+advturtle["findBlockID"] = native["findBlockID"]
 
+advturtle["findBlock"] = native["findBlock"]
 advturtle["findblock"] = native["findBlock"]
 
 local function selectBlock( block )
@@ -44,6 +96,7 @@ advturtle["selectBlockID"] = selectBlockID
 advturtle["selectblockid"] = selectBlockid
 
 advturtle["placeFront"] = turtle.place
+advturtle["placefront"] = turtle.place
 
 local function left( width )
 	if width == nil then width = 1 end
@@ -86,11 +139,22 @@ end
 advturtle["left"] = left
 advturtle["right"] = right
 
+local function getPos()
+	return { native["x"](), native["y"](), native["z"]() }
+end
+
+advturtle["getPos"] = getPos
+advturtle["getpos"] = getPos
+advturtle["pos"] = getPos
+
+advturtle["placefrontleftdown"] = native["placeFrontLeftDown"]
+advturtle["placefrontdown"] = native["placeFrontDown"]
+advturtle["placefrontrightdown"] = native["placeFrontRightDown"]
 
 for k,v in pairs( native ) do
 	if type( k ) == "string" and type( v ) == "function" then
 		if advturtle[k] == nil then
-			advturtle[k] = wrap( k )
+			advturtle[k] = adv_wrap( k )
 		end
 	end
 end
--- src/AdvTurtleAPI/TileMyEntityTurtle.java
+++ src/AdvTurtleAPI/TileMyEntityTurtle.java
@@ -20,29 +20,25 @@ import net.minecraft.src.dan200.turtle.shared.TileEntityTurtle;
 
 public class TileMyEntityTurtle extends TileEntityTurtle {
 
-	private static final int[] xOffsetLD = { 0, 0, -1, 1, -1, 1 };
-	private static final int[] yOffsetLD = { 0, 0, -1, -1, -1, -1 };
-	private static final int[] zOffsetLD = { 0, 0, -1, 1, 1, -1 };
-
 	private NetworkedComputerHelper m_computer = null;
 	private ItemStack[] m_inventory = null;
 	private PeripheralAdvTurtleAPI my_peripheral = null;
 
 	public TileMyEntityTurtle() {
 		super();
-		//System.out.println("TileMyEntityTurtle()");
+		// System.out.println("TileMyEntityTurtle()");
 		hook();
 	}
 
 	public TileMyEntityTurtle(int i) {
 		super(i);
-		//System.out.println("TileMyEntityTurtle(" + i + ")");
+		// System.out.println("TileMyEntityTurtle(" + i + ")");
 		hook();
 	}
 
 	public TileMyEntityTurtle(int j, int i) {
 		super(j, i);
-		//System.out.println("TileMyEntityTurtle(" + j + ", " + i + ")");
+		// System.out.println("TileMyEntityTurtle(" + j + ", " + i + ")");
 		hook();
 	}
 
@@ -89,6 +85,9 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 		return m_inventory;
 	}
 
+	/**
+	 * 
+	 */
 	private void updateCommands() {
 		try {
 			Integer animation = (Integer) getSuperField("m_clientState", "animation");
@@ -106,7 +105,8 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 					if (((Integer) peek).intValue() >= mod_AdvTurtleAPI.cmdOffset)
 						cmd = ((Integer) commandQueue.remove()).intValue();
 					else {
-						//System.out.println("Calling base command: " + ((Integer) peek).intValue());
+						// System.out.println("Calling base command: " +
+						// ((Integer) peek).intValue());
 						base = true;
 					}
 				}
@@ -130,7 +130,7 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 				if (cmd < 0)
 					return;
 
-				//System.out.println("Calling advturtle command: " + cmd);
+				System.out.println("Calling advturtle command: " + cmd);
 
 				boolean flag = false;
 
@@ -141,20 +141,109 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 					System.out.println("Unknown command: " + cmd);
 					break;
 
-				case 5: // placeFrontLeftDown
+				case 5: {// placeFrontLeftDown
+					final int[] xOffsetFLD = { 0, 0, -1, 1, -1, 1 };
+					final int[] yOffsetFLD = { 0, 0, -1, -1, -1, -1 };
+					final int[] zOffsetFLD = { 0, 0, -1, 1, 1, -1 };
+
 					int dir = (Integer) getSuperField("m_clientState", "dir");
-					flag = place(xOffsetLD[dir], yOffsetLD[dir], zOffsetLD[dir], getOppositeDir(dir));
+					flag = place(xOffsetFLD[dir], yOffsetFLD[dir], zOffsetFLD[dir], getOppositeDir(dir));
 
 					if (flag)
 						startAnimation(4);
 					break;
+				}
+				case 6: {// placeFrontDown
+					final int[] xOffsetFD = { 0, 0, 0, 0, -1, 1 };
+					final int[] yOffsetFD = { 0, 0, -1, -1, -1, -1 };
+					final int[] zOffsetFD = { 0, 0, -1, 1, 0, 0 };
+
+					int dir = (Integer) getSuperField("m_clientState", "dir");
+					flag = place(xOffsetFD[dir], yOffsetFD[dir], zOffsetFD[dir], getOppositeDir(dir));
+
+					if (flag)
+						startAnimation(4);
+					break;
+				}
+				case 7: {// placeFrontRightDown
+					final int[] xOffsetFRD = { 0, 0, 1, -1, -1, 1 };
+					final int[] yOffsetFRD = { 0, 0, -1, -1, -1, -1 };
+					final int[] zOffsetFRD = { 0, 0, -1, 1, -1, 1 };
+
+					int dir = (Integer) getSuperField("m_clientState", "dir");
+					flag = place(xOffsetFRD[dir], yOffsetFRD[dir], zOffsetFRD[dir], getOppositeDir(dir));
+
+					if (flag)
+						startAnimation(4);
+					break;
+				}
+				case 8: {// placeFrontLeft
+					final int[] xOffsetFL = { 0, 0, -1, 1, -1, 1 };
+					final int[] yOffsetFL = { 0, 0, 0, 0, 0, 0 };
+					final int[] zOffsetFL = { 0, 0, -1, 1, 1, -1 };
+
+					int dir = (Integer) getSuperField("m_clientState", "dir");
+					flag = place(xOffsetFL[dir], yOffsetFL[dir], zOffsetFL[dir], getOppositeDir(dir));
+
+					if (flag)
+						startAnimation(4);
+					break;
+				}
+				case 9: {// placeFrontRight
+					final int[] xOffsetFR = { 0, 0, 1, -1, -1, 1 };
+					final int[] yOffsetFR = { 0, 0, 0, 0, 0, 0 };
+					final int[] zOffsetFR = { 0, 0, -1, 1, -1, 1 };
+
+					int dir = (Integer) getSuperField("m_clientState", "dir");
+					flag = place(xOffsetFR[dir], yOffsetFR[dir], zOffsetFR[dir], getOppositeDir(dir));
+
+					if (flag)
+						startAnimation(4);
+					break;
+				}
+				case 10: {// placeFrontLeftUp
+					final int[] xOffsetFLU = { 0, 0, -1, 1, -1, 1 };
+					final int[] yOffsetFLU = { 0, 0, 1, 1, 1, 1 };
+					final int[] zOffsetFLU = { 0, 0, -1, 1, 1, -1 };
+
+					int dir = (Integer) getSuperField("m_clientState", "dir");
+					flag = place(xOffsetFLU[dir], yOffsetFLU[dir], zOffsetFLU[dir], getOppositeDir(dir));
+
+					if (flag)
+						startAnimation(4);
+					break;
+				}
+				case 11: {// placeFrontUp
+					final int[] xOffsetFU = { 0, 0, 0, 0, -1, 1 };
+					final int[] yOffsetFU = { 0, 0, 1, 1, 1, 1 };
+					final int[] zOffsetFU = { 0, 0, -1, 1, 0, 0 };
+
+					int dir = (Integer) getSuperField("m_clientState", "dir");
+					flag = place(xOffsetFU[dir], yOffsetFU[dir], zOffsetFU[dir], getOppositeDir(dir));
+
+					if (flag)
+						startAnimation(4);
+					break;
+				}
+				case 12: {// placeFrontRightUp
+					final int[] xOffsetFRU = { 0, 0, 1, -1, -1, 1 };
+					final int[] yOffsetFRU = { 0, 0, 1, 1, 1, 1 };
+					final int[] zOffsetFRU = { 0, 0, -1, 1, -1, 1 };
+
+					int dir = (Integer) getSuperField("m_clientState", "dir");
+					flag = place(xOffsetFRU[dir], yOffsetFRU[dir], zOffsetFRU[dir], getOppositeDir(dir));
+
+					if (flag)
+						startAnimation(4);
+					break;
+				}				
 				}
 
 				int commandsProcessed = (Integer) getSuperField("m_state", "commandsProcessed");
 				commandsProcessed++;
 				setSuperField("m_state", "commandsProcessed", commandsProcessed);
 
-				if(my_peripheral != null)
+				if (my_peripheral != null)
 					my_peripheral.commandProcessed(commandsProcessed, flag);
 			}
 		} catch (Exception e) {
@@ -251,10 +340,12 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 	public void hook() {
 		if (m_computer == null) {
 			try {
-				m_computer = (NetworkedComputerHelper) ModLoader.getPrivateValue(TileEntityTurtle.class, this, "m_computer");
+				m_computer = (NetworkedComputerHelper) ModLoader.getPrivateValue(TileEntityTurtle.class, this,
+						"m_computer");
 				my_peripheral = new PeripheralAdvTurtleAPI(this);
 				m_computer.addPeripheralAsAPI(my_peripheral);
-				//System.out.println("Successfully hooked " + this + " @" + m_computer);
+				// System.out.println("Successfully hooked " + this + " @" +
+				// m_computer);
 			} catch (Exception e) {
 				System.out.println("Hooking " + this + " failed!!!");
 				e.printStackTrace();
@@ -264,7 +355,8 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 		if (m_inventory == null) {
 			try {
 				m_inventory = (ItemStack[]) ModLoader.getPrivateValue(TileEntityTurtle.class, this, "m_inventory");
-				//System.out.println("Successfully got inventory @" + m_inventory + " of " + this);
+				// System.out.println("Successfully got inventory @" +
+				// m_inventory + " of " + this);
 			} catch (Exception e) {
 				System.out.println("Getting inventory of " + this + " failed!!!");
 				e.printStackTrace();
@@ -275,15 +367,15 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 	@Override
 	public void transferStateFrom(TileEntityTurtle tileentityturtle) {
 		System.out.println("transferStateFrom: " + this + " | " + tileentityturtle);
-		
+
 		super.transferStateFrom(tileentityturtle);
 
-		if(tileentityturtle instanceof TileMyEntityTurtle) {
-			m_computer = ((TileMyEntityTurtle)tileentityturtle).m_computer;
-			m_inventory = ((TileMyEntityTurtle)tileentityturtle).m_inventory;
-			my_peripheral = ((TileMyEntityTurtle)tileentityturtle).my_peripheral;
+		if (tileentityturtle instanceof TileMyEntityTurtle) {
+			m_computer = ((TileMyEntityTurtle) tileentityturtle).m_computer;
+			m_inventory = ((TileMyEntityTurtle) tileentityturtle).m_inventory;
+			my_peripheral = ((TileMyEntityTurtle) tileentityturtle).my_peripheral;
 		}
-		
+
 		hook();
 	}
 
@@ -291,7 +383,6 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 	public void readFromNBT(NBTTagCompound nbttagcompound) {
 		super.readFromNBT(nbttagcompound);
 		System.out.println("readFromNBT");
-		m_computer = null;
 		m_inventory = null;
 		hook();
 	}
@@ -312,7 +403,8 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 		Method takePlaceableItem;
 		ItemStack itemStack = null;
 		try {
-			takePlaceableItem = TileEntityTurtle.class.getDeclaredMethod("takePlaceableItem", int.class, int.class, int.class, int.class);
+			takePlaceableItem = TileEntityTurtle.class.getDeclaredMethod("takePlaceableItem", int.class, int.class,
+					int.class, int.class);
 			takePlaceableItem.setAccessible(true);
 			itemStack = (ItemStack) takePlaceableItem.invoke(this, new Object[] { x, y, z, m });
 		} catch (Exception e) {
@@ -337,9 +429,11 @@ public class TileMyEntityTurtle extends TileEntityTurtle {
 		Item item = Item.itemsList[itemstack.itemID];
 		Block block = Block.blocksList[itemstack.itemID];
 
-		//System.out.println("place: " + xOffset + "/" + yOffset + "/" + zOffset + ": " + itemstack.itemID + "@" + meta);
+		// System.out.println("place: " + xOffset + "/" + yOffset + "/" +
+		// zOffset + ": " + itemstack.itemID + "@" + meta);
 
-		if (worldObj.setBlockAndMetadataWithNotify(x, y, z, itemstack.itemID, item.getMetadata(itemstack.getItemDamage()))) {
+		if (worldObj.setBlockAndMetadataWithNotify(x, y, z, itemstack.itemID,
+				item.getMetadata(itemstack.getItemDamage()))) {
 			if (worldObj.getBlockId(x, y, z) == itemstack.itemID) {
 				block.onBlockPlaced(worldObj, x, y, z, meta);
 
